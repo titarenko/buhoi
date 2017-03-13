0f738d7ac8934a5a628cd218567bbd0033fedda1
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-13 23:10:10 +0200

    use korrekt as default validation framework

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ module.exports = {
 	log: require('./modules/log'),
 	router: require('./modules/router'),
 	security: require('./modules/security'),
+	server: require('./modules/server'),
+	validation: require('./modules/validation'),
 	webpack: require('./modules/webpack'),
 }
