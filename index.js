843f321467d78aabb5f892f32365eff8bfe9678f
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-13 23:15:54 +0200

    add loggers to exports

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
 	log: require('./modules/log'),
+	loggers: require('./modules/loggers'),
 	router: require('./modules/router'),
 	security: require('./modules/security'),
 	server: require('./modules/server'),
