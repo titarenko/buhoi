eedb778db3ce89da18023a5dde7aa89a435499c0
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-23 15:16:30 +0200

    add https server builder

--- modules/server.js
+++ modules/server.js
@@ -1,11 +1,37 @@
 const http = require('http')
 const httpShutdown = require('http-shutdown')
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+const constants = require('constants')
 
-module.exports = { http: createHttpServer }
+module.exports = {
+	http: createHttpServer,
+	https: createHttpsServer,
+}
 
-function createHttpServer (app, options) {
-	const signals = options && options.signals || ['SIGINT', 'SIGTERM']
-	const server = httpShutdown(http.createServer(app))
-	signals.forEach(it => process.on(it, () => server.shutdown()))
-	return server
+function createHttpServer (app) {
+	return app && app.redirectToHttps === true
+		? createHttpToHttpsServer()
+		: httpShutdown(http.createServer(app))
+}
+
+function createHttpToHttpsServer () {
+	return httpShutdown(http.createServer(redirectApp))
+	function redirectApp (req, res) {
+		const redirectLocation = `https://${req.headers['host']}${req.url}`
+		res.writeHead(301, { 'Location': redirectLocation })
+		res.end()
+	}
+}
+
+function createHttpsServer (app, { letsencrypt }) {
+	const options = {
+		key: fs.readFileSync(path.join(letsencrypt, 'privkey1.pem')),
+		cert: fs.readFileSync(path.join(letsencrypt, 'fullchain1.pem')),
+		ca: fs.readFileSync(path.join(letsencrypt, 'chain1.pem')),
+		dhparam: fs.readFileSync(path.join(letsencrypt, 'dh1.pem')),
+		secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_SSLv2,
+	}
+	return httpShutdown(https.createServer(app, options))
 }
--- tests/server.js
+++ tests/server.js
@@ -5,25 +5,18 @@ const shouldSinon_ = require('should-sinon')
 
 describe('server', function () {
 	describe('http', function () {
-		let instance, processOn, processStub, server
+		let instance, server
 		beforeEach(function () {
-			processOn = sinon.spy()
-			processStub = sinon.stub(process, 'on', processOn)
 			instance = { shutdown: sinon.spy() }
 			mockRequire('http-shutdown', () => instance)
 			server = mockRequire.reRequire('../modules/server')
 		})
 		afterEach(function () {
 			mockRequire.stopAll()
-			processStub.restore()
 		})
 		it('should create http server with graceful shutdown included', function () {
 			server.http((req_, res_) => undefined)
-			processOn.should.be.calledWith('SIGINT')
-			processOn.should.be.calledWith('SIGTERM')
-			processOn.getCall(0).args[1]()
-			processOn.getCall(1).args[1]()
-			instance.shutdown.should.be.calledTwice()
+			instance.shutdown.should.be.Function()
 		})
 	})
 })
