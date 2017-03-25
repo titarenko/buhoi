813dc762171af50a47d7fc21be1821d31efa1069
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-03-25 19:40:39 +0200

    move migrator to dedicated package

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
 	log: require('./modules/log'),
-	migrator: require('./modules/migrator'),
 	router: require('./modules/router'),
 	security: require('./modules/security'),
 	server: require('./modules/server'),
