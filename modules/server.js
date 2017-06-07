1d4c6e01ddf875d331233a04d50f9cab78b8ba77
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-06-07 13:56:31 +0300

    add server and proxy factories

--- modules/server.js
+++ modules/server.js
@@ -4,10 +4,13 @@ const https = require('https')
 const fs = require('fs')
 const path = require('path')
 const constants = require('constants')
+const httpProxy = require('http-proxy')
 
 module.exports = {
 	http: createHttpServer,
 	https: createHttpsServer,
+	createServer,
+	createProxy,
 }
 
 function createHttpServer (app) {
@@ -34,4 +37,64 @@ function createHttpsServer (app, { letsencrypt }) {
 		secureOptions: constants.SSL_OP_NO_SSLv3 | constants.SSL_OP_NO_SSLv2,
 	}
 	return httpShutdown(https.createServer(options, app))
+}
+
+function createServer (app, options) {
+	if (options) {
+		const http = createHttpServer({ redirectToHttps: true })
+		const https = createHttpsServer(app, options)
+		process.on('SIGINT', () => {
+			http.shutdown()
+			https.shutdown()
+		})
+		return { http, https }
+	} else {
+		const http = createHttpServer(app)
+		process.on('SIGINT', () => http.shutdown())
+		return { http, https: null }
+	}
+}
+
+function createProxy (targets, options) {
+	const proxy = httpProxy.createProxyServer()
+
+	if (options) {
+		const http = createHttpServer({ redirectToHttps: true })
+		const https = createHttpsServer(proxyHttp, options)
+		https.on('upgrade', proxyWs)
+		process.on('SIGINT', () => {
+			http.shutdown()
+			https.shutdown()
+		})
+		return { http, https }
+	} else {
+		const http = createHttpServer(proxyHttp)
+		http.on('upgrade', proxyWs)
+		process.on('SIGINT', () => http.shutdown())
+		return { http, https: null }
+	}
+
+	function proxyHttp (req, res) {
+		const target = getProxyTarget(targets, req)
+		if (target) {
+			proxy.web(req, res, { target })
+		} else {
+			res.sendStatus(404)
+		}
+	}
+
+	function proxyWs (req, socket, head) {
+		const target = getProxyTarget(targets, req)
+		if (target) {
+			proxy.ws(req, socket, head, { target })
+		} else {
+			socket.destroy()
+		}
+	}
+}
+
+function getProxyTarget (targets, req) {
+	const host = req.headers['host']
+	const name = host.slice(0, host.indexOf('.'))
+	return targets[name] || targets.find(x => x.isDefault)
 }
