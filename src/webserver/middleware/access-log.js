f7663861a99f675041f28f5d77b2b175df3c26bb
Constantin Titarenko <constantin.titarenko@gmail.com>
2017-12-26 21:54:45 +0200

    fix header name

--- src/webserver/middleware/access-log.js
+++ src/webserver/middleware/access-log.js
@@ -2,7 +2,7 @@ const Stream = require('totlog/stream')
 const morgan = require('morgan')
 
 module.exports = function accessLog (options) {
-  const format = ':status :response-time ms :remote-addr :req[session] :method :url'
+  const format = ':status :response-time ms :remote-addr :req[cookie] :method :url'
   const stream = new Stream(options)
   return morgan(format, { stream })
 }
