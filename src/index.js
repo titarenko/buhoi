4027953b2fffab74dc00b3b9a1294c4902f7a1de
Constantin Titarenko <constantin.titarenko@gmail.com>
2018-01-02 12:57:07 +0200

    modify default paths

--- src/index.js
+++ src/index.js
@@ -19,9 +19,9 @@ module.exports = {
 
 function start (options = { }) {
   _.extend({
-    featuresPath: `${__dirname}/../../features`,
-    publicPath: `${__dirname}/../../public`,
-    webpackConfigPath: `${__dirname}/../../pages/webpack.config.js`,
+    featuresPath: `${__dirname}/../../src/features`,
+    publicPath: `${__dirname}/../../src/public`,
+    webpackConfigPath: `${__dirname}/../../src/pages/webpack.config.js`,
   }, options)
 
   infra.initialize()
