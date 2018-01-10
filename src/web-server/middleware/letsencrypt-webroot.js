219aa95c8e1e81bcb8516da80d1ce600d64260c9
Constantin Titarenko <constantin.titarenko@gmail.com>
2018-01-10 21:39:11 +0200

    add missing prefix

--- src/web-server/middleware/letsencrypt-webroot.js
+++ src/web-server/middleware/letsencrypt-webroot.js
@@ -4,7 +4,7 @@ module.exports = function letsencryptWebroot () {
   const { BUHOI_WEBROOT } = process.env
   const router = express.Router()
   if (BUHOI_WEBROOT) {
-    router.use('/.well-known/acme-challenge', express.static(BUHOI_WEBROOT))
+    router.use('/.well-known/acme-challenge', express.static(`${BUHOI_WEBROOT}/.well-known/acme-challenge`))
   }
   return router
 }
