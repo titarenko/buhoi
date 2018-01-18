614e629e749970dd8ddf74fc8aa14185a8a55634
Constantin Titarenko <constantin.titarenko@gmail.com>
2018-01-18 13:22:33 +0200

    add subscription feature for storage

--- src/client/storage.js
+++ src/client/storage.js
@@ -1,5 +1,7 @@
 /* eslint-env browser */
 
+const listeners = []
+
 export function get (key, defaultValue) {
   try {
     const value = JSON.parse(localStorage[key])
@@ -11,8 +13,24 @@ export function get (key, defaultValue) {
 
 export function set (key, value) {
   localStorage[key] = JSON.stringify(value)
+  listeners.forEach(l => l(key, value))
 }
 
 export function remove (key) {
   delete localStorage[key]
+  listeners.forEach(l => l(key, undefined))
+}
+
+export function on (name, handler) {
+  if (name !== 'change') {
+    return
+  }
+  listeners.push(handler)
+}
+
+export function off (name, handler) {
+  if (name !== 'change') {
+    return
+  }
+  listeners.splice(listeners.indexOf(handler), 1)
 }
