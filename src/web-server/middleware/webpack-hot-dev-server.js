5035b65941543e8f2dfb53f5c49cf5572deaafa6
Constantin Titarenko <constantin.titarenko@gmail.com>
2018-08-08 10:52:39 +0300

    add switch to disable webpack dev middleware (closes #7)

--- src/web-server/middleware/webpack-hot-dev-server.js
+++ src/web-server/middleware/webpack-hot-dev-server.js
@@ -3,7 +3,9 @@ const { Router } = require('express')
 module.exports = function webpackHotDevServer (webpackConfigPath) {
   const router = Router()
 
-  if (webpackConfigPath && process.env.NODE_ENV === 'development') {
+  if (webpackConfigPath &&
+    process.env.NODE_ENV === 'development' &&
+    !process.env.BUHOI_DISABLE_WEBPACK_DEV_MIDDLEWARE) {
     const webpackDevMiddleware = require('webpack-dev-middleware')
     const webpackHotMiddleware = require('webpack-hot-middleware')
     const webpack = require('webpack')
