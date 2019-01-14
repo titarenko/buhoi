84e993b0628ebeb157022dc1ad27a891f4da82de
Constantin Titarenko <constantin.titarenko@gmail.com>
2019-01-14 11:15:20 +0200

    add logging of task server message handler errors

--- src/infra/mq.js
+++ src/infra/mq.js
@@ -1,12 +1,34 @@
 const mqu = require('mqu')
+const totlog = require('totlog')
 
-module.exports = { initialize, terminate }
+class MessageHandlerFailureError extends Error {
+  constructor ({ event, task, error }) {
+    super('task server message handler failure')
+    this.event = event
+    this.task = task
+    this.initialError = error
+  }
+}
+
+module.exports = { initialize, terminate, MessageHandlerFailureError }
 
 function initialize () {
   const { BUHOI_MQ } = process.env
 
   if (BUHOI_MQ) {
-    return mqu(BUHOI_MQ)
+    const instance = mqu(BUHOI_MQ)
+    const log = totlog(__filename)
+    instance.on('error', error => {
+      if (error instanceof MessageHandlerFailureError) {
+        if (error.event) {
+          log.error('failed to handle event %s due to', error.event, error.initialError)
+        } else {
+          log.error('failed to execute task %s due to', error.task, error.initialError)
+        }
+      } else {
+        throw error
+      }
+    })
   }
 }
 
