ca97091fa65737266bf6c15ba0f73820d46c0584
Constantin Titarenko <constantin.titarenko@gmail.com>
2019-03-29 11:42:27 +0200

    add headers to prevent index.html caching

--- src/web-server/middleware/html5-history-fallback.js
+++ src/web-server/middleware/html5-history-fallback.js
@@ -3,6 +3,9 @@ const assert = require('assert')
 module.exports = function html5HistoryFallback (publicPath) {
   assert(typeof publicPath, 'string')
   return function html5HistoryFallbackMiddleware (req, res, next) {
+    res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
+    res.set('Pragma', 'no-cache')
+    res.set('Expires', '0')
     return res.sendFile(`${publicPath}/index.html`)
   }
 }
