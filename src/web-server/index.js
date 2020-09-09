5d155e93a52c914554d41a130616f3a650469e2f
Constantin Titarenko <constantin.titarenko@gmail.com>
2020-09-09 22:01:03 +0300

    allow disable web server

--- src/web-server/index.js
+++ src/web-server/index.js
@@ -6,11 +6,17 @@ module.exports = { start, stop }
 let app, server
 
 function start (options) {
+  if (process.env.BUHOI_DISABLE_WEB) {
+    return
+  }
   app = application.create(options)
   server = transport.create(app)
 }
 
 async function stop () {
+  if (process.env.BUHOI_DISABLE_WEB) {
+    return
+  }
   await transport.dispose(server)
   await application.dispose(app)
 }
